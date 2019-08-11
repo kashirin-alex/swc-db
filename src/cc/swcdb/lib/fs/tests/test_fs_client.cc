88635014f16b4208331161c4dff81346d174e414
Kashirin Alex <kashirin.alex@gmail.com>
2019-08-11 22:18:03 +0300

    append in 64MB blocks and sync in middle

--- src/cc/swcdb/lib/fs/tests/test_fs_client.cc
+++ src/cc/swcdb/lib/fs/tests/test_fs_client.cc
@@ -44,7 +44,7 @@ void run(size_t thread_id){
     FS::DirentList listing;
     EnvFsInterface::fs()->readdir(err, "", listing);
     if(err != Error::OK){ 
-     std::cerr << "ERROR readir err=" << err << "\n";
+     std::cerr << "ERROR readir err=(" << err << ")\n";
      exit(1);
     }
     bool found = false;
@@ -135,20 +135,36 @@ void run(size_t thread_id){
     }
     
     // append >> 
-    std::string data("Abc1234");
+    size_t file_sz = 0;
+    std::string data_start("Abc1234");
     std::string data_end("567890");
-    std::string data_str = data;
-    int sz = 536870912-data_str.length()-data_end.length();
-    for(int i=0;i<sz;i++)
-      data.append("+");
-    data.append(data_end);
-
-    StaticBuffer buffer(data.data(), data.length(), false);
-    size_t amount = EnvFsInterface::fs()->append(err, smartfd, buffer, FS::Flags::FLUSH);
-    if(err != Error::OK || amount!=data.length() || smartfd->pos() != data.length()) { 
-     std::cerr << "ERROR(append) err=" << err << " amount=" << amount << " " << smartfd->to_string() <<"\n";
-     exit(1);
+    int r=1;
+    size_t file_blk = 67108864*r-data_start.length()-data_end.length();
+    size_t written = 0;
+    for(int i=0;i<16/r;i++){
+      file_sz += 67108864*r;
+      std::string data = data_start;
+      for(int i=0;i<file_blk;i++)
+        data.append("+");
+      data.append(data_end);
+      StaticBuffer buffer(data.data(), data.length(), false);
+      size_t amount = EnvFsInterface::fs()->append(err, smartfd, buffer, FS::Flags::FLUSH);
+      written += amount;
+      if(err != Error::OK || amount!=data.length() || smartfd->pos() != written) { 
+        std::cerr << "ERROR(append) err=" << err << " amount=" << amount << " " << smartfd->to_string() <<"\n";
+        exit(1);
+      }
+      if(i == 7) {
+        err = Error::OK;
+        EnvFsInterface::fs()->sync(err, smartfd);
+        if(err != Error::OK){ 
+          std::cerr << "ERROR(sync,append) err=" << err << "\n";
+          exit(1);
+        }
+      }
+      std::cout << " progress=" << thread_id << " written=" << written << "\n";
     }
+
 
     // flush >>
     err = Error::OK;
@@ -184,8 +200,8 @@ void run(size_t thread_id){
     // length >>
     err = Error::OK;
     size_t len = EnvFsInterface::fs()->length(err, smartfd->filepath());
-    if(err != Error::OK || len != data.length()) { 
-     std::cerr << "ERROR(length) len=" << len << " expected-len=" << data.length() 
+    if(err != Error::OK || len != file_sz) { 
+     std::cerr << "ERROR(length) len=" << len << " expected-len=" << file_sz
                << " err=" << err << " " << smartfd->to_string() <<"\n";
      exit(1);
     }
@@ -201,10 +217,10 @@ void run(size_t thread_id){
 
     // read >>
     err = Error::OK;
-    uint8_t buf[data_str.length()];
-    if (EnvFsInterface::fs()->read(err, smartfd, buf,  data_str.length()) != data_str.length() 
+    uint8_t buf[data_start.length()];
+    if (EnvFsInterface::fs()->read(err, smartfd, buf,  data_start.length()) != data_start.length() 
         || err != Error::OK 
-        || strcmp((char*)buf, data_str.c_str()) != 0) { 
+        || strcmp((char*)buf, data_start.c_str()) != 0) { 
      std::cerr << "ERROR(read) err=" << err << " buf=" << buf << " " << smartfd->to_string() <<"\n";
      exit(1);
     }
@@ -251,24 +267,49 @@ void run(size_t thread_id){
 
 int main(int argc, char** argv) {
   EnvConfig::init(argc, argv);
-  EnvIoCtx::init(8);
 
+  EnvIoCtx::init(8);
   EnvFsInterface::init();
-    
-  for(size_t chk=1;chk<=1;chk++) {
+  
+  for(size_t chk=1;chk<=2;chk++) {
+    int err = Error::OK;
+    // make data-root
+    EnvFsInterface::fs()->mkdirs(err, "");
+    if(err != Error::OK){ 
+      std::cerr << "ERROR(make data-root) mkdirs err=" << err << "\n";
+      exit(1);
+    }
+
     std::cout << "--1--\n";
     std::vector<std::thread*> threads;
-    for(size_t t=1;t<=8;t++)
+    for(size_t t=1;t<=2;t++)
       threads.push_back(new std::thread([t](){run(t);}));
     
     std::cout << "--2--\n";
     for(auto t : threads) t->join();
-
     std::cout << "--3--\n";
+
   
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    err = Error::OK;
+    // remove data-root
+    EnvFsInterface::fs()->rmdir(err, "");
+    if(err != Error::OK){ 
+      std::cerr << "ERROR(rmr data-root) rmdir err=" << err << "\n";
+      exit(1);
+    }
+    
     std::cout << "--######################-- chk=" <<chk<< "\n";
+    //std::this_thread::sleep_for(std::chrono::milliseconds(10000));
   }
+  
+  //for(size_t chk=1;chk<=10;chk++)
+  //  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  
+  //std::cout << " fs()->stop\n";
+  EnvFsInterface::fs()->stop();
+  
+  //for(size_t chk=1;chk<=100;chk++)
+  //  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   
   exit(0);
 }
