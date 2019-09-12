2bc3249f650896bf8e464433c423d6b6801ca790
Kashirin Alex <kashirin.alex@gmail.com>
2019-09-12 13:27:41 +0300

    remove structured ID path's sub tree if empty

--- src/cc/swcdb/lib/db/Columns/MNGR/Column.h
+++ src/cc/swcdb/lib/db/Columns/MNGR/Column.h
@@ -33,7 +33,7 @@ class Column : public std::enable_shared_from_this<Column> {
   }
 
   static bool remove(int &err, int64_t id) {
-    Env::FsInterface::interface()->rmdir(err, Range::get_column_path(id));
+    Env::FsInterface::interface()->rmdir_incl_opt_subs(err, Range::get_column_path(id));
     return true;
   }
 
