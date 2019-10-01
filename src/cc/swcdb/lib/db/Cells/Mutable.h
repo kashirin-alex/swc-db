c829f037d6d6af4dceb89b95c5205eb5bcd75250
Kashirin Alex <kashirin.alex@gmail.com>
2019-10-02 00:20:56 +0300

    added class Cells::Mutable + tests

---
+++ src/cc/swcdb/lib/db/Cells/Mutable.h
@@ -0,0 +1,313 @@
+/*
+ * Copyright (C) 2019 SWC-DB (author: Kashirin Alex (kashirin.alex@gmail.com))
+ */
+
+
+#ifndef swcdb_lib_db_Cells_Mutable_h
+#define swcdb_lib_db_Cells_Mutable_h
+
+#include <mutex>
+
+#include "swcdb/lib/db/Cells/Cell.h"
+#include "SpecsInterval.h"
+
+namespace SWC { namespace DB { namespace Cells {
+
+
+class Mutable {
+  public:
+
+  typedef std::shared_ptr<Mutable>  Ptr;
+  
+  inline static Ptr make(const uint32_t cap=1, 
+                         const uint32_t max_revs=1, 
+                         const uint64_t ttl=0, 
+                         const Types::Column type=Types::Column::PLAIN) {
+    return std::make_shared<Mutable>(cap, max_revs, ttl, type);
+  }
+
+  explicit Mutable(const uint32_t cap=1, 
+                   const uint32_t max_revs=1, 
+                   const uint64_t ttl=0, 
+                   const Types::Column type=Types::Column::PLAIN)
+                  : m_cells(0), m_cap(cap==0?1:cap), m_size(0), 
+                    m_max_revs(max_revs), m_ttl(ttl), m_type(type) {
+    _allocate();
+  }
+
+  virtual ~Mutable() {
+    std::cout << " ~Mutable " << (size_t)this << " 1\n";
+    _free();
+    std::cout << " ~Mutable " << (size_t)this << " 2\n";
+  }
+
+  void allocate_if_needed(uint32_t sz) {
+    std::lock_guard<std::mutex> lock(m_mutex);
+    return _allocate_if_needed(sz);
+  }
+
+  void add(Cell& e_cell){ 
+    //std::cout << "add, " << e_cell.to_string() << "\n";
+    
+    if(e_cell.has_expired(m_ttl))
+      return;
+
+    uint32_t size;
+    Cell* cell;
+    uint32_t revs = 0;
+    bool removing = false;
+    bool updating = false;
+    uint32_t on_fraction;
+    {
+      //std::lock_guard<std::mutex> lock(m_mutex);
+      size = m_size;
+    }
+
+    for(uint32_t offset = 0; offset < size; offset++) {
+      //std::lock_guard<std::mutex> lock(m_mutex);
+
+      if(size != m_size)  {
+        offset = 0;
+        size = m_size;
+        continue;
+      }
+
+      cell = *(m_cells + offset);
+      
+      if(cell->has_expired(m_ttl)) {
+         _move_bwd(offset--, 1);
+        size = m_size;
+        continue;
+      }
+
+      Condition::Comp cond = cell->key.compare(
+        e_cell.key, 
+        (removing || updating) ?  e_cell.on_fraction : cell->on_fraction
+      );
+      
+      if(cond == Condition::EQ) {
+
+        if(removing) {
+          if(e_cell.is_removing(cell->revision)){
+            _move_bwd(offset--, 1);
+            size = m_size;
+          }
+          continue;
+          
+        } else if(cell->removal()) {
+          if(cell->is_removing(e_cell.revision))
+            return;
+          continue;
+        }
+
+        if(e_cell.removal()) {
+          removing = true;
+          _insert(offset++, e_cell);
+          size = m_size;
+          continue;
+        
+        } else {
+
+          if(m_type == Types::Column::COUNTER_I64) {
+            
+            if(!updating && e_cell.on_fraction) {
+              updating = true;
+              _insert(offset++, e_cell);
+              size = m_size;
+              continue;
+            }
+
+            OP op1;
+            int64_t v1 = cell->get_value(&op1);
+            OP op2;
+            int64_t v2 = e_cell.get_value(&op2);
+
+            if(op1 == OP::EQUAL && op2 == OP::EQUAL 
+              && cell->revision > e_cell.revision)
+              return;
+
+            v2 = op2 == OP::EQUAL? v2 :(v1+(op2 == OP::MINUS?-v2:+v2));
+            if(cell->on_fraction) {
+              //std::cout << "cell->on_fraction\n";
+              e_cell.set_value(OP::EQUAL, v2);
+              if(e_cell.revision < cell->revision)
+                e_cell.revision = cell->revision;
+              if(e_cell.timestamp < cell->timestamp)
+                e_cell.timestamp = cell->timestamp;
+              continue;
+            }
+
+            cell->set_value(OP::EQUAL, v2);
+            if(cell->revision < e_cell.revision)
+              cell->revision = e_cell.revision;
+            if(cell->timestamp < e_cell.timestamp)
+              cell->timestamp = e_cell.timestamp;
+            
+            //std::cout << cell->key.to_string() << " == " << cell->get_value(&op2) << "\n";
+            if(updating)
+              continue;
+            return;
+
+          } else {
+
+            if(cell->revision == e_cell.revision) {
+              cell->copy(e_cell);
+              return;
+            }
+
+            if(m_max_revs == revs)
+              return;
+
+            if(e_cell.control & TS_DESC 
+              ? e_cell.timestamp < cell->timestamp
+              : e_cell.timestamp > cell->timestamp) {
+              if(m_max_revs == ++revs) {
+                //std::cout << "revs=" << revs << "\n";
+                //std::cout << "old " << cell->to_string() << "\n";
+                //std::cout << "new " << e_cell.to_string() << "\n";
+                return;
+              }
+              continue;
+            }
+          }
+
+        } 
+
+      } else if(removing || updating)
+        return;
+
+      if(cond == Condition::GT)
+        continue;
+
+      _insert(offset, e_cell);
+      return;
+    }
+
+    push_back(e_cell);
+  }
+  
+  void push_back(const Cell& cell) {
+    //std::lock_guard<std::mutex> lock(m_mutex);
+    _allocate_if_needed(1);
+    *(m_cells + m_size) = new Cell(cell);
+    m_size++;
+  }
+  
+  void insert(uint32_t offset, const Cell& cell) {
+    //std::lock_guard<std::mutex> lock(m_mutex);
+    _insert(offset, cell);
+  }
+
+  uint32_t size(){
+    std::lock_guard<std::mutex> lock(m_mutex);
+    return m_size;
+  }
+
+  void free(){
+    std::lock_guard<std::mutex> lock(m_mutex);
+    _free();
+  }
+
+  bool scan(const Specs::Interval& specs, DynamicBufferPtr result){
+    //
+    return false;
+  }
+
+  const std::string to_string() {
+    std::lock_guard<std::mutex> lock(m_mutex);
+    return _to_string();
+  }
+  
+  private:
+
+  inline const std::string _to_string() {
+    std::string s("CellsMutable(cap=");
+    s.append(std::to_string(m_cap));
+    s.append(" size=");
+    s.append(std::to_string(m_size));
+    s.append(" max_revs=");
+    s.append(std::to_string(m_max_revs));
+    s.append(" ttl=");
+    s.append(std::to_string(m_ttl));
+    s.append(")");
+    return s;
+  }
+
+  inline void _allocate_if_needed(uint32_t sz) {
+    if(m_cap < m_size + sz){
+      m_cap += sz;
+      //auto ts = Time::now_ns();
+      _allocate();
+      //std::cout << " _allocate took=" << Time::now_ns()-ts << " " << _to_string() << "\n";
+    }
+  }
+
+  void _allocate() {
+    if(m_cells == 0) {
+      m_cells = new Cell*[m_cap];
+
+    } else {
+      Cell** cells_old = m_cells;
+      m_cells = new Cell*[m_cap += m_size];
+
+      memcpy(m_cells, cells_old, m_size*sizeof(Cell*));
+      //for(uint32_t n=m_size;n--;) 
+      //  *(m_cells+n) = *(cells_old+n);
+
+      delete [] cells_old;
+    }
+
+    memset(m_cells+m_size, 0, (m_cap-m_size)*sizeof(Cell*));
+    //for(uint32_t n=m_cap-m_size; n--;) 
+    //  *(m_cells+m_size+n) = nullptr;
+  }
+
+  inline void _move_fwd(uint32_t offset, int32_t by) {
+    _allocate_if_needed(by);
+    memmove(m_cells+offset+by, m_cells+offset, (m_size-offset)*sizeof(Cell*));
+    //Cell** end = m_cells+m_size+by-1;
+    //for(uint32_t n = m_size-offset;n--;)
+      //*end-- = *(end-by); 
+  }
+
+  inline void _move_bwd(uint32_t offset, int32_t by) {
+    for(uint32_t n=0;n<by; n++)
+      if(*(m_cells+offset+n))
+        delete *(m_cells+offset+n);
+
+    memmove(m_cells+offset, m_cells+offset+by, (m_size-offset-by)*sizeof(Cell*));
+    m_size-=by;
+    memset(m_cells+m_size, 0, (m_cap-m_size)*sizeof(Cell*));
+  }
+  
+  inline void _insert(uint32_t offset, const Cell& cell) {
+    _move_fwd(offset, 1);
+    *(m_cells + offset) = new Cell(cell);
+    m_size++;
+  }
+
+  inline void _free() {
+    //std::cout << " _free, m_cells " << (size_t)*m_cells << " " << _to_string() << "\n";
+    if(m_cells != 0) {
+      while(m_size) 
+        delete *(m_cells+(m_size--));
+      delete [] m_cells;
+      m_cap = 0;
+      m_cells = 0;
+    }
+    //std::cout << " _free 2\n";
+  }
+
+  std::mutex        m_mutex;
+  Cell**            m_cells;
+  uint32_t          m_cap;
+  uint32_t          m_size;
+  uint32_t          m_max_revs;
+  uint64_t          m_ttl;
+  Types::Column     m_type;
+};
+
+
+
+}}}
+#endif
---
+++ src/cc/swcdb/lib/db/Cells/tests/test_cells_mutable.cc
@@ -0,0 +1,195 @@
+/*
+ * Copyright (C) 2019 SWC-DB (author: Kashirin Alex (kashirin.alex@gmail.com))
+ */
+
+
+#include <thread>
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+#include "swcdb/lib/db/Cells/Mutable.h"
+#include "swcdb/lib/db/Cells/Chain.h"
+#include "swcdb/lib/db/Stats/Stat.h"
+
+
+namespace Cells = SWC::DB::Cells;
+
+void check(size_t num_cells) {
+  std::cout << " checking with cells=" << num_cells << "\n";
+      
+
+  //std::cout << "\n--------Init Cells-------------\n";
+  std::shared_ptr<SWC::Stats::Stat> latency_vector = std::make_shared<SWC::Stats::Stat>();
+  int64_t ts_total_vector = 0;
+  int64_t took_vector;
+
+  int64_t rev;
+  std::vector<Cells::Cell*> cells;
+  std::string value;
+  Cells::Cell cell;
+  
+  /*
+  std::shared_ptr<SWC::Stats::Stat> latency = std::make_shared<SWC::Stats::Stat>();
+  int64_t ts_total = 0;
+  int64_t took;
+  Cells::Chain::Ptr cells_chain  = std::make_shared<Cells::Chain>(1);
+  */
+
+  std::shared_ptr<SWC::Stats::Stat> latency_mutable = std::make_shared<SWC::Stats::Stat>();
+  int64_t ts_total_mutable = 0;
+  int64_t took_mutable;
+  Cells::Mutable::Ptr cells_mutable = Cells::Mutable::make(1, 2, 0, SWC::Types::Column::COUNTER_I64);
+  
+  int num_revs = 50;
+  for(auto r=1;r<=num_revs;++r){
+  for(auto i=0;i<num_cells;++i){
+      //std::cout << "Initial Cell-"<< i << ":\n";
+      std::string n = std::to_string(i);
+      
+      rev = SWC::Time::now_ns();
+      cell.flag = Cells::INSERT;
+      cell.set_timestamp(rev-1);
+      cell.set_revision(rev);
+      cell.set_time_order_desc(false);
+
+      cell.key.free();
+      cell.key.add("aKey1");
+      cell.key.add("aKey2");
+      cell.key.add(n);
+      cell.key.add("aKey3");
+      cell.key.add("aKey4");
+      cell.key.add("aKey5");
+      if(num_revs == r)
+        cell.set_value(Cells::OP::EQUAL, 0);
+      else
+        cell.set_value(Cells::OP::PLUS, 1);
+      //value = std::string("A-Data-Value-1234567890-"+n);
+      //cell.set_value(value.data(), value.length());
+
+      took_vector = SWC::Time::now_ns();
+
+      cells.insert(cells.begin(), new Cells::Cell(cell));
+      //cells.push_back(new Cells::Cell(cell));
+
+      took_vector = SWC::Time::now_ns()-took_vector;
+      ts_total_vector += took_vector;
+      latency_vector->add(took_vector); 
+      
+      /*
+      took = SWC::Time::now_ns();
+
+      cells_chain->add(cell);
+
+      took = SWC::Time::now_ns()-took;
+      ts_total += took;
+      latency->add(took); 
+      */
+      
+      took_mutable = SWC::Time::now_ns();
+
+      //cells_mutable->insert(0, cell);
+      cells_mutable->add(cell);
+
+      took_mutable = SWC::Time::now_ns()-took_mutable;
+      ts_total_mutable += took_mutable;
+      latency_mutable->add(took_mutable); 
+
+      if(i % 10000 == 0)
+        std::cout << "took=" << took_mutable << " " << r << "/" << i << " = " << cells_mutable->size() << "\n";
+
+      //std::cout << cells.back()->to_string() << "\n\n";
+  }
+  }
+
+  std::cout << " vector "
+            << " avg="    << latency_vector->avg()
+            << " min="    << latency_vector->min()
+            << " max="    << latency_vector->max()
+            << " count="  << latency_vector->count()
+            << " median=" << ts_total_vector/num_cells
+            << " took="   << ts_total_vector / 1000000
+            << " counting, ";
+  took_vector = SWC::Time::now_ns();
+  int64_t tmpc = 0;
+  for(auto& cell: cells){
+    if(cell->vlen >= 0)
+      tmpc++;
+  }
+  std::cout << " counted=" << tmpc
+            << " took=" << SWC::Time::now_ns()-took_vector << "\n"; 
+  
+  /// 
+  /*
+  std::cout << " chained"
+            << " avg="    << latency->avg()
+            << " min="    << latency->min()
+            << " max="    << latency->max()
+            << " count="  << latency->count()
+            << " median=" << ts_total/latency->count()
+            << " counting, ";
+  took = SWC::Time::now_ns();
+  std::cout << " counted=" << cells_chain->count()
+            << " took=" << SWC::Time::now_ns()-took << "\n"; 
+
+  cells_chain =  nullptr;
+  */
+  /// 
+
+  std::cout << " mutable"
+            << " avg="    << latency_mutable->avg()
+            << " min="    << latency_mutable->min()
+            << " max="    << latency_mutable->max()
+            << " count="  << latency_mutable->count()
+            << " median=" << ts_total_mutable / num_cells
+            << " took="   << ts_total_mutable / 1000000
+            << " counting, ";
+  took_mutable = SWC::Time::now_ns();
+  std::cout << " counted=" << cells_mutable->size()
+            << " took=" << SWC::Time::now_ns()-took_mutable<< "\n"; 
+
+  //cells_mutable->free();
+  
+  
+  std::cout << " clearing, ";
+
+  cells_mutable = nullptr;
+
+  for(auto & cell : cells) {
+    cell->own = true;
+    delete cell;
+  }
+  cells.clear();
+
+  std::cout << " cleared check with cells=" << num_cells << "\n";
+
+}
+  
+int main(int argc, char** argv) {
+  
+  check(1);
+  check(10);
+  check(100);
+  check(1000);
+  check(10000);
+
+  check(100000);
+  check(100000);
+  check(100000);
+
+  check(1000000);
+  check(1000000);
+  check(1000000);
+
+  
+  //for(auto i=1; i<=100; i++) {
+  //  check(5000000);
+  //  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  //}
+
+
+  //for(auto i=1; i<=10000000; i*=10)
+  //  check(i);
+
+  exit(0);
+}
