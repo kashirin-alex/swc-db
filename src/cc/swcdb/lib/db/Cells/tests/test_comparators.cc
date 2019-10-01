d1c312ecf2eb693842f84f2610e859aad209079d
Kashirin Alex <kashirin.alex@gmail.com>
2019-10-02 00:28:55 +0300

    output difference for negative

--- src/cc/swcdb/lib/db/Cells/tests/test_comparators.cc
+++ src/cc/swcdb/lib/db/Cells/tests/test_comparators.cc
@@ -18,7 +18,7 @@ int main() {
     a1 = "A1";
     a2 = "A1B11111";
     if(!Condition::is_matching(Condition::PF, a1, strlen(a1), a2, strlen(a2)))
-       { std::cout << "PF ERROR!"; exit(1);}
+       { std::cout << "~PF ERROR!"; exit(1);}
     // prefix negative
     a1 = "A1";
     a2 = "A2";
@@ -30,7 +30,7 @@ int main() {
     a1 = "A1";
     a2 = "A1B11111";
     if(!Condition::is_matching(Condition::GT, a1, strlen(a1), a2, strlen(a2))) 
-       { std::cout << "GT ERROR!"; exit(1);}
+       { std::cout << "~GT ERROR!"; exit(1);}
     // greater negative
     a1 = "Ab";
     a2 = "Aa";
@@ -41,7 +41,7 @@ int main() {
     a1 = "A1";
     a2 = "A1";
     if(!Condition::is_matching(Condition::GE, a1, strlen(a1), a2, strlen(a2))) 
-       { std::cout << "GE ERROR!"; exit(1);}
+       { std::cout << "~GE ERROR!"; exit(1);}
     // greater equal negative
     a1 = "A2";
     a2 = "A1";
@@ -52,7 +52,7 @@ int main() {
     a1 = "A1";
     a2 = "A1";
     if(!Condition::is_matching(Condition::EQ, a1, strlen(a1), a2, strlen(a2))) 
-       { std::cout << "EQ ERROR!"; exit(1);}
+       { std::cout << "~EQ ERROR!"; exit(1);}
     // equal negative
     a1 = "A2";
     a2 = "A1";
@@ -63,7 +63,7 @@ int main() {
     a1 = "A1000";
     a2 = "A0";
     if(!Condition::is_matching(Condition::LE, a1, strlen(a1), a2, strlen(a2))) 
-       { std::cout << "LE ERROR!"; exit(1);}
+       { std::cout << "~LE ERROR!"; exit(1);}
     // lower equal negative
     a1 = "A2";
     a2 = "A2222";
@@ -74,7 +74,7 @@ int main() {
     a1 = "A1";
     a2 = "A0";
     if(!Condition::is_matching(Condition::LT, a1, strlen(a1), a2, strlen(a2))) 
-       { std::cout << "LT ERROR!"; exit(1);}
+       { std::cout << "~LT ERROR!"; exit(1);}
     // lower negative
     a1 = "A0";
     a2 = "A0";
@@ -85,7 +85,7 @@ int main() {
     a1 = "A1";
     a2 = "A0";
     if(!Condition::is_matching(Condition::NE, a1, strlen(a1), a2, strlen(a2))) 
-       { std::cout << "NE ERROR!"; exit(1);}
+       { std::cout << "~NE ERROR!"; exit(1);}
     // not equal negative
     a1 = "A0";
     a2 = "A0";
@@ -96,7 +96,7 @@ int main() {
     a1 = "^A.*0$";
     a2 = "A5432454350";
     if(!Condition::is_matching(Condition::RE, a1, strlen(a1), a2, strlen(a2))) 
-       { std::cout << "RE ERROR!"; exit(1);}
+       { std::cout << "~RE ERROR!"; exit(1);}
     // not regexp negative
     a1 = "^A.*1$";
     a2 = "A5432454350";
