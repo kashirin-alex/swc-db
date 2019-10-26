7f1f931ac23774a5cb1597b313169d9bb4bb0824
Kashirin Alex <kashirin.alex@gmail.com>
2019-10-27 00:12:18 +0300

    added configuration get/set helpers method

--- src/cc/swcdb/lib/db/Types/Encoding.h
+++ src/cc/swcdb/lib/db/Types/Encoding.h
@@ -11,6 +11,7 @@ namespace SWC { namespace Types {
 
 
 enum class Encoding {
+  DEFAULT = 0x00,
   PLAIN   = 0x01,
   ZLIB    = 0x02,
   SNAPPY  = 0x03
@@ -19,15 +20,34 @@ enum class Encoding {
 const std::string to_string(Encoding typ) {
   switch(typ){
     case Encoding::PLAIN:
-      return std::string("plain");
+      return std::string("PLAIN");
     case Encoding::ZLIB:
-      return std::string("zlib");
+      return std::string("ZLIB");
     case Encoding::SNAPPY:
-      return std::string("snappy");
+      return std::string("SNAPPY");
     default:
-      return std::string("unknown(" + std::to_string((uint8_t)typ) +")");
+      return std::string("UMKNOWN(" + std::to_string((uint8_t)typ) +")");
   }
 }
+
+const std::string repr_encoding(int typ) {
+  return to_string((Encoding)typ);
+}
+
+const int from_string_encoding(std::string typ) {
+  std::transform(typ.begin(), typ.end(), typ.begin(), 
+        [](unsigned char c){ return std::toupper(c); }
+  );
+
+  if(typ.compare("ZLIB") == 0)
+    return (int)Encoding::ZLIB;
+
+  if(typ.compare("SNAPPY") == 0)
+    return (int)Encoding::SNAPPY;
+  
+  return (int)Encoding::PLAIN;
+}
+
 }}
 
 #endif // swc_lib_db_types_Encoding_h
