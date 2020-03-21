729e0caf0222356d5109f5489e3299a58c310c62
Kashirin Alex <kashirin.alex@gmail.com>
2020-03-21 02:05:03 +0200

    added class Semaphore

---
+++ src/cc/include/swcdb/core/Semaphore.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright Since 2019 SWC-DB© [author: Kashirin Alex kashirin.alex@gmail.com]
+ */
+
+#ifndef swc_core_Semaphore_h
+#define swc_core_Semaphore_h
+
+
+#include <mutex>
+#include <condition_variable>
+
+
+namespace SWC { 
+  
+class Semaphore final {
+  public:
+
+  Semaphore(size_t sz=1);
+
+  ~Semaphore();
+
+  void acquire();
+
+  void release();
+
+  void wait_until_under_and_acquire(size_t sz);
+
+  void wait_until_under(size_t sz);
+
+  void wait_all();
+
+  private:
+  std::mutex                m_mutex;
+  std::condition_variable   m_cv;
+  const size_t              m_sz;
+  size_t                    m_count;
+};
+
+}
+
+
+#ifdef SWC_IMPL_SOURCE
+#include "swcdb/core/Semaphore.cc"
+#endif 
+
+#endif // swc_core_Semaphore_h
---
+++ src/cc/lib/swcdb/core/Semaphore.cc
@@ -0,0 +1,52 @@
+/*
+ * Copyright Since 2019 SWC-DB© [author: Kashirin Alex kashirin.alex@gmail.com]
+ */
+
+
+#include "swcdb/core/Semaphore.h"
+
+namespace SWC { 
+  
+Semaphore::Semaphore(size_t sz) 
+          : m_sz(sz), m_count(0) {
+}
+
+Semaphore::~Semaphore() {
+  m_cv.notify_all();
+} 
+
+void Semaphore::acquire() {
+  std::unique_lock lock_wait(m_mutex);
+  if(m_count >= m_sz)
+    m_cv.wait(lock_wait, [this] {return m_count < m_sz;});
+  ++m_count;
+}
+
+void Semaphore::release() {
+  {
+    std::unique_lock lock_wait(m_mutex);
+    --m_count;
+  }
+  m_cv.notify_all();
+}
+
+void Semaphore::wait_until_under_and_acquire(size_t sz) {
+  std::unique_lock lock_wait(m_mutex);
+  if(m_count >= sz)
+    m_cv.wait(lock_wait, [this, sz] {return m_count < sz;});
+  ++m_count;
+}
+
+void Semaphore::wait_until_under(size_t sz) {
+  std::unique_lock lock_wait(m_mutex);
+  if(m_count >= sz)
+    m_cv.wait(lock_wait, [this, sz] {return m_count < sz;});
+}
+
+void Semaphore::wait_all() {
+  std::unique_lock lock_wait(m_mutex);
+  if(m_count)
+    m_cv.wait(lock_wait, [this] {return !m_count;});
+}
+
+}
