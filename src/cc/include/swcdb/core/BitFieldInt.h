fdeae09a2d23c74bb60883ce97923809564cc16f
Kashirin Alex <kashirin.alex@gmail.com>
2020-04-09 12:35:31 +0300

    added u/int40_t, u/int48_t, u/int56_t types

--- src/cc/include/swcdb/core/BitFieldInt.h
+++ src/cc/include/swcdb/core/BitFieldInt.h
@@ -11,6 +11,7 @@
 
 namespace SWC {
 
+
 #pragma pack(push, 1)
 template <class T, uint8_t SZ>
 struct BitFieldInt final {
@@ -224,6 +225,7 @@ struct BitFieldInt final {
   */
 
 };
+#pragma pack(pop)
 
 
 template<typename T, uint8_t SZ>
@@ -352,10 +354,18 @@ SWC_BITFIELD1_vs_BITFIELD2(>);
 
 
 typedef BitFieldInt<uint32_t, 24> uint24_t;
-typedef BitFieldInt<int32_t, 24>  int24_t;
+typedef BitFieldInt<int32_t,  24> int24_t;
+
+typedef BitFieldInt<uint64_t, 40> uint40_t;
+typedef BitFieldInt<int64_t,  40> int40_t;
+
+typedef BitFieldInt<uint64_t, 48> uint48_t;
+typedef BitFieldInt<int64_t,  48> int48_t;
+
+typedef BitFieldInt<uint64_t, 56> uint56_t;
+typedef BitFieldInt<int64_t,  56> int56_t;
 
 
-#pragma pack(pop)
 } // namespace SWC
 
 
@@ -363,12 +373,34 @@ typedef BitFieldInt<int32_t, 24>  int24_t;
 
 typedef SWC::uint24_t uint24_t;
 typedef SWC::int24_t  int24_t;
+static const uint24_t UINT24_MAX = UINT32_MAX >> 8;   // 16777215
+static const uint24_t UINT24_MIN = UINT24_MAX + 1;    // 0 
+static const int24_t   INT24_MAX = UINT24_MAX / 2;    // 8388607
+static const int24_t   INT24_MIN = INT24_MAX + 1;     // -8388608
 
-static const uint24_t UINT24_MAX = UINT32_MAX >> 1; // 16777215
-static const uint24_t UINT24_MIN = UINT24_MAX + 1;  // 0 
-static const int24_t   INT24_MAX = UINT24_MAX / 2;  // 8388607
-static const int24_t   INT24_MIN = INT24_MAX + 1;   // -8388608
 
+typedef SWC::uint40_t uint40_t;
+typedef SWC::int40_t  int40_t;
+static const uint40_t UINT40_MAX = UINT64_MAX >> 24;  // 1099511627775
+static const uint40_t UINT40_MIN = UINT40_MAX + 1;    // 0 
+static const int40_t   INT40_MAX = UINT40_MAX / 2;    // 549755813887
+static const int40_t   INT40_MIN = INT40_MAX + 1;     // -549755813888
+
+
+typedef SWC::uint48_t uint48_t;
+typedef SWC::int48_t  int48_t;
+static const uint48_t UINT48_MAX = UINT64_MAX >> 16;  // 281474976710655
+static const uint48_t UINT48_MIN = UINT48_MAX + 1;    // 0 
+static const int48_t   INT48_MAX = UINT48_MAX / 2;    // 140737488355327
+static const int48_t   INT48_MIN = INT48_MAX + 1;     // -140737488355328
+
+
+typedef SWC::uint56_t uint56_t;
+typedef SWC::int56_t  int56_t;
+static const uint56_t UINT56_MAX = UINT64_MAX >> 8;  // 72057594037927935
+static const uint56_t UINT56_MIN = UINT56_MAX + 1;    // 0 
+static const int56_t   INT56_MAX = UINT56_MAX / 2;    // 36028797018963967
+static const int56_t   INT56_MIN = INT56_MAX + 1;     // -36028797018963968
 
 
 # undef SWC_CAN_INLINE 
