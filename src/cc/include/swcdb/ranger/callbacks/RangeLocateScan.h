ab63cbd5eb7707aded810011fb0f13e49a270091
Kashirin Alex <kashirin.alex@gmail.com>
2020-04-13 18:46:15 +0300

    fixed reached_limits() is if params set

--- src/cc/include/swcdb/ranger/callbacks/RangeLocateScan.h
+++ src/cc/include/swcdb/ranger/callbacks/RangeLocateScan.h
@@ -26,6 +26,8 @@ class RangeLocateScan : public ReqScan {
   virtual ~RangeLocateScan() { }
 
   bool selector(const DB::Cells::Cell& cell, bool& stop) override {
+    //SWC_PRINT << "---------------------"
+    //  << "  cell.key: " << cell.key.to_string() << SWC_PRINT_CLOSE;
     if(any_is && spec.range_begin.compare(cell.key, any_is) != Condition::EQ)
       return false;
 
@@ -36,7 +38,7 @@ class RangeLocateScan : public ReqScan {
     if(cell.key.count > any_is && spec.range_end.count > any_is && 
        !spec.is_matching_end(cell.key)) {
       stop = true;
-      //std::cout << "-- KEY-BEGIN NO MATCH STOP --\n";
+      //SWC_PRINT << "-- KEY-BEGIN NO MATCH STOP --" << SWC_PRINT_CLOSE;
       return false;
     }
 
@@ -47,7 +49,7 @@ class RangeLocateScan : public ReqScan {
 
     if(key_end.count > any_is && spec.range_begin.count > any_is && 
        !spec.is_matching_begin(key_end)) {
-      //std::cout << "-- KEY-END NO MATCH --\n";
+      //SWC_PRINT << "-- KEY-END NO MATCH --" << SWC_PRINT_CLOSE;
       return false;
     }
     //return true; // without aligned min-max
@@ -59,14 +61,14 @@ class RangeLocateScan : public ReqScan {
     DB::Cell::KeyVec aligned_max;
     aligned_max.decode(&ptr, &remain);
     /*
-    std::cout << "---------------------\n";
-    std::cout << "range_begin: " << spec.range_begin.to_string() << "\n";
-    std::cout << "  key_begin: " << cell.key.to_string() << "\n";
-    std::cout << "aligned_min: " << aligned_min.to_string() << "\n";
-    std::cout << "  range_end: " << spec.range_end.to_string() << "\n";
-    std::cout << "    key_end: " << key_end.to_string() << "\n";
-    std::cout << "aligned_max: " << aligned_max.to_string() << "\n";
-    std::cout << "        rid: " << rid << "\n";
+    SWC_PRINT 
+      << "range_begin: " << spec.range_begin.to_string() << "\n"
+      << "  key_begin: " << cell.key.to_string() << "\n"
+      << "aligned_min: " << aligned_min.to_string() << "\n"
+      << "  range_end: " << spec.range_end.to_string() << "\n"
+      << "    key_end: " << key_end.to_string() << "\n"
+      << "aligned_max: " << aligned_max.to_string() << "\n"
+      << "        rid: " << rid << SWC_PRINT_CLOSE;
     */
     if(spec.range_begin.count == any_is || aligned_max.empty() || 
        spec.range_begin.compare(
@@ -74,17 +76,16 @@ class RangeLocateScan : public ReqScan {
       if(spec.range_end.count == any_is || aligned_min.empty() || 
          spec.range_end.compare(
            aligned_min, Condition::GT, spec.range_end.count, true)) {
-        //std::cout << "-- ALIGNED MATCH  --\n";
+        //SWC_PRINT << "-- ALIGNED MATCH  --" << SWC_PRINT_CLOSE;
         return true;
       }
     }
-    //std::cout << "-- ALIGNED NO MATCH -------\n";
+    //SWC_PRINT << "-- ALIGNED NO MATCH -------" << SWC_PRINT_CLOSE;
     return false;
   }
 
   bool reached_limits() override {
-    // if meta-data rsp to be more than one
-    return true;
+    return params.cid && params.rid;
   }
 
   bool add_cell_and_more(const DB::Cells::Cell& cell) override {
@@ -103,7 +104,7 @@ class RangeLocateScan : public ReqScan {
     }
     params.range_begin.remove(0);
     params.range_end.remove(0);
-    return !reached_limits();
+    return false;
   }
 
   bool add_cell_set_last_and_more(const DB::Cells::Cell& cell) override {
