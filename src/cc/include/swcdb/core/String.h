bbc543fed7670045dabba2072325e7722ccac9ad
Kashirin Alex <kashirin.alex@gmail.com>
2020-06-25 17:54:51 +0300

    chged format by pre-calculated size

--- src/cc/include/swcdb/core/String.h
+++ src/cc/include/swcdb/core/String.h
@@ -1,71 +1,42 @@
 /*
  * Copyright Since 2019 SWC-DB© [author: Kashirin Alex kashirin.alex@gmail.com]
- * Copyright (C) 2007-2016 Hypertable, Inc.
- *
- * This file is part of Hypertable.
- *
- * Hypertable is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 3
- * of the License, or any later version.
- *
- * Hypertable is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Hypertable. If not, see <http://www.gnu.org/licenses/>
  */
 
 
-#ifndef swc_common_STRING_H
-#define swc_common_STRING_H
+#ifndef swc_core_Sring_h
+#define swc_core_Sring_h
+
 
 #include <string>
 #include <sstream>
-#include <limits>
+
 
 namespace SWC {
 
-  /**
-   * Returns a std::string using printf like format facilities
-   * Vanilla snprintf is about 1.5x faster than this, which is about:
-   *   10x faster than boost::format;
-   *   1.5x faster than std::string append (operator+=);
-   *   3.5x faster than std::string operator+;
-   *
-   * @param fmt A printf-like format string
-   * @return A new std::string with the formatted text
-   */
-  std::string format(const char *fmt, ...) __attribute__((format (printf, 1, 2)));
 
-  /**
-   * Return a string presentation of a sequence. Is quiet slow but versatile,
-   * as it uses ostringstream.
-   *
-   * @param seq A STL-compatible sequence with forward-directional iterators
-   * @param sep A separator which is inserted after each list item
-   * @return A new std::string with the formatted text
-   */
+  std::string format(const char *fmt, ...);
+
+
   template <class SequenceT>
   std::string format_list(const SequenceT &seq, const char *sep = ", ") {
-    typedef typename SequenceT::const_iterator Iterator;
-    Iterator it = seq.begin(), end = seq.end();
+    if(seq.empty())
+      return "[]";
+
+    auto it = seq.begin();
+    auto end = seq.end();
+
     std::ostringstream out;
     out << '[';
-
-    if (it != end) {
+    more:
       out << *it;
-      ++it;
-    }
-    for (; it != end; ++it)
-      out << sep << *it;
-
+      if(++it != end) {
+        out << sep;
+        goto more;
+      }
     out << ']';
+
     return out.str();
   }
-
 
 
 }
--- src/cc/lib/swcdb/core/String.cc
+++ src/cc/lib/swcdb/core/String.cc
@@ -1,21 +1,5 @@
 /*
  * Copyright Since 2019 SWC-DB© [author: Kashirin Alex kashirin.alex@gmail.com]
- * Copyright (C) 2007-2016 Hypertable, Inc.
- *
- * This file is part of Hypertable.
- *
- * Hypertable is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 3
- * of the License, or any later version.
- *
- * Hypertable is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Hypertable. If not, see <http://www.gnu.org/licenses/>
  */
 
 
@@ -24,43 +8,25 @@
 
 #include <cstdarg>
 #include <cstdio>
-#include <algorithm>
 
 
 namespace SWC {
 
+
 std::string format(const char *fmt, ...) {
-  char buf[1024];       // should be enough for most cases
-  int n, size = sizeof(buf);
-  char *p = buf;
-  va_list ap;
-
-  do {
-    va_start(ap, fmt);
-    n = vsnprintf(p, size, fmt, ap);
-    va_end(ap);
-
-    if (n > -1 && n < size)
-      break;    // worked!
-
-    if (n > -1)         // glibc 2.1+/iso c99
-      size = n + 1;     //   exactly what's needed
-    else                // glibc 2.0
-      size *= 2;        //   double the size and try again
-
-    p = (char *)(p == buf ? malloc(size) : realloc(p, size));
-
-    if (!p)
-      throw std::bad_alloc();
-  } while (true);
-
-  if (buf == p)
-    return std::string(p, n);
-
-  std::string ret(p, n);
-  free(p);
-
-  return ret;
+  std::string res;
+  
+  std::va_list ap1; 
+  std::va_list ap2; 
+  va_start(ap1, fmt);
+  va_copy(ap2, ap1);
+  
+  res.resize(std::vsnprintf(nullptr, 0, fmt, ap1));
+  std::vsnprintf(res.data(), res.size() + 1, fmt, ap2);
+  
+  va_end(ap1);
+  va_end(ap2);
+  return res;
 }
 
 
