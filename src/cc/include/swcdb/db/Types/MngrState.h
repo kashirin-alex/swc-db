b05f9193663d8f4b4fcb952ce5b606a97f1c58a9
Kashirin Alex <kashirin.alex@gmail.com>
2020-08-10 08:11:36 +0300

    added std::string Types::to_string(MngrState )

--- src/cc/include/swcdb/db/Types/MngrState.h
+++ src/cc/include/swcdb/db/Types/MngrState.h
@@ -7,18 +7,28 @@
 #ifndef swc_db_types_MngrState_h
 #define swc_db_types_MngrState_h
 
+#include <string>
 
 namespace SWC { namespace Types { 
 
+
 enum MngrState {
-  NOTSET = 0,
-  OFF = 1,
-  STANDBY = 2,
-  WANT = 3,
+  NOTSET    = 0,
+  OFF       = 1,
+  STANDBY   = 2,
+  WANT      = 3,
   NOMINATED = 4,
-  ACTIVE = 5
+  ACTIVE    = 5
 };
 
+
+std::string to_string(MngrState state);
+
+
 }}
+
+#ifdef SWC_IMPL_SOURCE
+#include "swcdb/db/Types/MngrState.cc"
+#endif 
 
 #endif // swc_db_types_MngrState_h
---
+++ src/cc/lib/swcdb/db/Types/MngrState.cc
@@ -0,0 +1,41 @@
+
+/*
+ * Copyright Since 2019 SWC-DB© [author: Kashirin Alex kashirin.alex@gmail.com]
+ * License details at <https://github.com/kashirin-alex/swc-db/#license>
+ */
+
+
+#include "swcdb/db/Types/MngrState.h"
+
+
+namespace SWC { namespace Types {
+
+
+std::string to_string(MngrState state) {
+  switch(state) {
+
+    case MngrState::NOTSET:
+      return std::string("NOTSET");
+
+    case MngrState::OFF:
+      return std::string("OFF");
+
+    case MngrState::STANDBY:
+      return std::string("STANDBY");
+
+    case MngrState::WANT:
+      return std::string("WANT");
+
+    case MngrState::NOMINATED:
+      return std::string("NOMINATED");
+
+    case MngrState::ACTIVE:
+      return std::string("ACTIVE");
+
+    default:
+      return std::string("uknown");
+  }
+}
+
+
+}}
