f96c66df23f9b3b400f37a069cd13345ac16bd3a
Kashirin Alex <kashirin.alex@gmail.com>
2020-08-10 08:17:43 +0300

    added shell::Mngr command report managers status

--- src/cc/include/swcdb/utils/cli/Shell_Manager.h
+++ src/cc/include/swcdb/utils/cli/Shell_Manager.h
@@ -23,6 +23,16 @@ class Mngr : public Interface {
 
     options.push_back(
       new Option(
+        "status", 
+        {"report managers status by mngr-endpoint, of Schema-Role or by All",
+        "status [Schemas(default) | ALL | endpoint=HOST/IP(|PORT)];"},
+        [ptr=this](std::string& cmd){return ptr->managers_status(cmd);}, 
+        new re2::RE2("(?i)^(report|status)(\\s|$)")
+      )
+    );
+
+    options.push_back(
+      new Option(
         "column-status", 
         {"report column status with ranges status",
         "column-status [cid=CID | name=NAME];"},
@@ -47,6 +57,97 @@ class Mngr : public Interface {
         std::make_shared<client::AppContext>()
       )
     );
+  }
+
+  bool managers_status(std::string& cmd) {
+    std::string message;
+
+    size_t at = cmd.find_first_of(" ");
+    cmd = cmd.substr(at+1);
+    client::SQL::Reader reader(cmd, message);
+    while(reader.found_space());
+
+    client::Mngr::Hosts hosts;
+    if(reader.found_token("endpoint", 8)) {
+      while(reader.found_space());
+      reader.expect_eq();
+      if(reader.err)
+        return error(message);
+
+      std::string host_or_ips;
+      reader.read(host_or_ips, " |");
+      if(reader.err)
+        return error(message);
+      if(host_or_ips.empty())
+        return error("Missing Endpoint");
+      
+      while(reader.found_space());
+      uint16_t port;
+      bool was_set = false;
+      if(*(reader.ptr-1) == '|' || reader.found_token("|", 1)) {
+        reader.read_uint16_t(port, was_set);
+        if(reader.err)
+          return error(message);
+      } 
+      if(!was_set)
+        port = Env::Config::settings()->get_i16("swc.mngr.port");
+      
+      std::vector<std::string> ips;
+      std::string host;
+      if(Resolver::is_ipv4_address(host_or_ips) || 
+         Resolver::is_ipv6_address(host_or_ips))
+        ips.push_back(host_or_ips);
+      else
+        host = host_or_ips; 
+
+      hosts.push_back(
+        Resolver::get_endpoints(
+          port, ips, host, {}, false
+        )
+      );
+      if(hosts.back().empty())
+        return error("Missing Endpoint");
+
+    } else if(reader.found_token("all", 3)) {
+      auto groups = Env::Clients::get()->mngrs_groups->get_groups();
+      for(auto& g : groups) {
+        auto tmp =  g->get_hosts();
+        hosts.insert(hosts.end(), tmp.begin(), tmp.end());
+      }
+
+    } else { // reader.found_token("schemas", 7)
+      hosts.push_back({});
+    }
+
+    for(auto& endpoints : hosts) {
+      std::promise<void>  r_promise;
+      Protocol::Mngr::Req::ManagersStatus::request(
+        endpoints,
+        [this, await=&r_promise] 
+        (const client::ConnQueue::ReqBase::Ptr& req, const int& error,
+         const Protocol::Mngr::Params::Report::RspManagersStatus& rsp) {
+          if(!(err = error)) {
+            Mutex::scope lock(Logger::logger.mutex);
+            std::cout << "# by Manager(" 
+                      << (req->queue 
+                          ? req->queue->get_endpoint_remote() 
+                          : EndPoint() 
+                          ) << ") ";
+            rsp.display(std::cout);
+            std::cout << '\n';
+          }
+          await->set_value();
+        }
+      );
+      r_promise.get_future().wait();
+
+      if(err) {
+        message.append(Error::get_text(err));
+        message.append("\n");
+        return error(message);
+      }
+    }
+    return true;
   }
 
   bool column_status(std::string& cmd) {
@@ -174,6 +275,7 @@ class Mngr : public Interface {
     }
     return true;
   }
+
 };
 
 
