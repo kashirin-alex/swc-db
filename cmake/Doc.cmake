13408ed43260f8347f34b914df61a4f1906b60c2
Kashirin Alex <kashirin.alex@gmail.com>
2020-09-24 20:13:23 +0300

    fixed target doc & extended Doxyfile & added logos
    The Doxygen Generated SWC-DB Documentations:
     - https://www.swcdb.org/doxygen/

--- cmake/Doc.cmake
+++ cmake/Doc.cmake
@@ -6,21 +6,21 @@ if(SWC_DOCUMENTATION)
 
   find_package(Doxygen REQUIRED)
 
-  configure_file(Doxyfile ${CMAKE_CURRENT_BINARY_DIR} @ONLY)
+  configure_file(${CMAKE_MODULE_PATH}/Doxyfile ${CMAKE_CURRENT_BINARY_DIR} @ONLY)
   add_custom_target(doc ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
 
 
 
   if(THRIFT_COMPILER_FOUND)
 
-    add_custom_target(doc_thrift thrift -gen html
+    add_custom_target(doc_thrift_html thrift -gen html
                       -o ${CMAKE_CURRENT_BINARY_DIR}/doc
                       ${CMAKE_CURRENT_SOURCE_DIR}/src/thrift/swcdb/Service.thrift)
     add_custom_command(TARGET doc POST_BUILD COMMAND make doc_thrift_html)
 
     ## require - thrift t_markdown_generator
     #   https://github.com/kashirin-alex/thrift/commit/b371bbf9750f196f21d244b8c3d58527c8e3961b
-    ## add_custom_target(doc_thrift thrift -gen markdown:suffix=md
+    ## add_custom_target(doc_thrift_md thrift -gen markdown:suffix=md
     ##                   -o ${CMAKE_CURRENT_BINARY_DIR}/doc
     ##                   ${CMAKE_CURRENT_SOURCE_DIR}/src/thrift/swcdb/Service.thrift)
     ## add_custom_command(TARGET doc POST_BUILD COMMAND make doc_thrift_md)
@@ -29,9 +29,8 @@ if(SWC_DOCUMENTATION)
 
 
   add_custom_command(TARGET doc POST_BUILD COMMAND 
-                     tar --xz
-                     --directory=${CMAKE_CURRENT_BINARY_DIR}
-                     --create --file=swc-db-doc.tar.xz 
-                     doc )
+                     XZ_OPT=-e9 tar -cJf swc-db-doc.tar.xz doc/*
+                     )
+
 
 endif ()
--- docs/build/documentations/README.md
+++ docs/build/documentations/README.md
@@ -17,6 +17,8 @@ cd swcdb;
 ```
 
 **The 'doc' target will generate an Archive "swc-db-doc.tar.xz" in the root build directory with Doxygen & Thrift-Compiler source documentations.**
+> * [The Doxygen Generated SWC-DB Documentations](https://www.swcdb.org/doxygen/)
+
 ```bash
 make doc;
 ```
