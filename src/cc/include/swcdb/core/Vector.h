2bdcc1e91a5442027118b00b89813905a3e657d0
Kashirin Alex <kashirin.alex@gmail.com>
2021-06-24 02:32:42 +0300

    added Core::Vector<T, SizeT>
    [TEST COMMIT]

---
+++ src/cc/include/swcdb/core/Vector.h
@@ -0,0 +1,439 @@
+/*
+ * SWC-DB© Copyright since 2019 Alex Kashirin <kashirin.alex@gmail.com>
+ * License details at <https://github.com/kashirin-alex/swc-db/#license>
+ */
+
+#ifndef swcdb_core_Vector_h
+#define swcdb_core_Vector_h
+
+#include <vector>
+
+namespace SWC { namespace Core {
+
+template<typename T, typename SizeT=uint32_t>
+class Vector {
+  public:
+
+  using value_type          = T;
+  using size_type           = SizeT;
+
+  typedef value_type*       pointer;
+  typedef const pointer     const_pointer;
+
+  typedef value_type*       iterator;
+  typedef const iterator    const_iterator;
+
+  typedef value_type&       reference;
+  typedef const reference   const_reference;
+
+
+  SWC_CAN_INLINE
+  static constexpr size_type max_size() noexcept {
+    return size_type(0) - size_type(1);
+  }
+
+
+  SWC_CAN_INLINE
+  Vector() noexcept : _data(nullptr), _cap(0), _size(0) { }
+
+  SWC_CAN_INLINE
+  Vector(size_type sz)
+          : _data(sz ? _allocate(sz) : nullptr), _cap(sz), _size(sz) {
+  }
+
+  SWC_CAN_INLINE
+  Vector(Vector&& other) noexcept
+        : _data(other._data), _cap(other._cap), _size(other._size) {
+    other._data = nullptr;
+    other._cap = other._size = 0;
+  }
+
+  SWC_CAN_INLINE
+  Vector(const Vector& other)
+        : _data(
+            other._size
+              ? _copy(
+                  _allocate_uinitialized(other._size),
+                  other._data, other._size
+                )
+              : nullptr
+          ),
+          _cap(other._size), _size(other._size) {
+  }
+
+  SWC_CAN_INLINE
+  ~Vector() {
+    if(_data)
+      _deallocate(_data, _cap);
+  }
+
+  SWC_CAN_INLINE
+  void free() noexcept {
+    if(_data) {
+      _deallocate(_data, _cap);
+      _data = nullptr;
+      _size = _cap = 0;
+    }
+  }
+
+
+  SWC_CAN_INLINE
+  Vector& operator=(Vector&& other) noexcept {
+    _data = other._data;
+    _cap = other._cap;
+    _size = other._size;
+    other._data = nullptr;
+    other._cap = other._size = 0;
+    return *this;
+  }
+
+  SWC_CAN_INLINE
+  Vector& operator=(const Vector& other) {
+    free();
+    if((_cap = _size = other._size)) {
+      _data = _copy(_allocate_uinitialized(_size), other._data, _size);
+    }
+    return *this;
+  }
+
+
+  SWC_CAN_INLINE
+  bool empty() const noexcept {
+    return !_size;
+  }
+
+  SWC_CAN_INLINE
+  size_type size() const noexcept {
+    return _size;
+  }
+
+  SWC_CAN_INLINE
+  size_type capacity() const noexcept {
+    return _cap;
+  }
+
+
+  SWC_CAN_INLINE
+  pointer data() noexcept {
+    return _data;
+  }
+
+  SWC_CAN_INLINE
+  pointer data() const noexcept {
+    return _data;
+  }
+
+
+  SWC_CAN_INLINE
+  iterator begin() noexcept {
+    return _data;
+  }
+
+  SWC_CAN_INLINE
+  iterator cbegin() const noexcept {
+    return _data;
+  }
+
+  SWC_CAN_INLINE
+  iterator begin() const noexcept {
+    return cbegin();
+  }
+
+
+  SWC_CAN_INLINE
+  iterator end() noexcept {
+    return _data + _size;
+  }
+
+  SWC_CAN_INLINE
+  iterator cend() const noexcept {
+    return _data + _size;
+  }
+
+  SWC_CAN_INLINE
+  iterator end() const noexcept {
+    return cend();
+  }
+
+
+  SWC_CAN_INLINE
+  reference front() noexcept {
+    return *_data;
+  }
+
+  SWC_CAN_INLINE
+  const_reference front() const noexcept {
+    return *_data;
+  }
+
+
+  SWC_CAN_INLINE
+  reference back() noexcept {
+    return _data[_size - 1];
+  }
+
+  SWC_CAN_INLINE
+  const_reference back() const noexcept {
+    return _data[_size - 1];
+  }
+
+
+  SWC_CAN_INLINE
+  reference operator[](size_type pos) noexcept {
+    return _data[pos];
+  }
+
+  SWC_CAN_INLINE
+  const_reference operator[](size_type pos) const noexcept {
+    return _data[pos];
+  }
+
+
+  SWC_CAN_INLINE
+  void clear() noexcept {
+    for(pointer ptr = _data; _size; --_size, ++ptr)
+      ptr->~value_type();
+  }
+
+  void shrink_to_fit(size_type sz=0) {
+    if(_cap > sz || _cap > _size) {
+      if(_size) {
+        _cap = sz > _size ? sz : _size;
+        _data = _allocate_uinitialized(_data, _size, _cap);
+      } else {
+        free();
+      }
+    }
+  }
+
+  SWC_CAN_INLINE
+  void reserve(size_type sz) {
+    if(_cap < sz)
+      _data = _allocate_uinitialized(_data, _size, (_cap = sz));
+  }
+
+  template<typename... ArgsT>
+  SWC_CAN_INLINE
+  void resize(size_type sz, ArgsT&&... args) {
+    if(sz > _size) {
+      reserve(sz);
+      for(pointer ptr = _data + _size; _size < sz; ++_size, ++ptr)
+        _construct(ptr, std::forward<ArgsT>(args)...);
+    } else if(sz < _size) {
+      for(pointer ptr = _data + sz; sz < _size; --_size, ++ptr)
+        ptr->~value_type();
+    }
+  }
+
+
+  template<typename... ArgsT>
+  SWC_CAN_INLINE
+  void push_back(ArgsT&&... args) {
+    reserve(_size + 1);
+    _construct(_data + _size, std::forward<ArgsT>(args)...);
+    ++_size;
+  }
+
+  template<typename... ArgsT>
+  SWC_CAN_INLINE
+  reference emplace_back(ArgsT&&... args) {
+    reserve(_size + 1);
+    reference ref = *_construct(_data + _size, std::forward<ArgsT>(args)...);
+    ++_size;
+    return ref;
+  }
+
+  template<typename... ArgsT>
+  SWC_CAN_INLINE
+  iterator insert(const_iterator it, ArgsT&&... args) {
+    size_type offset;
+    if(!_size || (offset = it - _data) == _size)
+      return &emplace_back(std::forward<ArgsT>(args)...);
+
+    reserve(_size + 1);
+    return _construct(
+      _alter(_data + offset, (_size++) - offset, 1), std::forward<ArgsT>(args)...);
+  }
+
+  SWC_CAN_INLINE
+  iterator insert(const_iterator it, iterator first, iterator last) {
+    if(!_size) {
+      assign(first, last);
+      return _data;
+    }
+
+    size_type offset = it - _data;
+    size_type amt = last - first;
+    reserve(_size + amt);
+
+    pointer data_offset = _alter(_data + offset, _size - offset, amt);
+    _size += amt;
+    for(pointer ptr=data_offset; first < last; ++first, ++ptr)
+      _construct(ptr, *first);
+    return data_offset;
+  }
+
+  SWC_CAN_INLINE
+  void assign(iterator first, iterator last) {
+    clear();
+    reserve(last - first);
+    _size = last - first;
+    for(pointer ptr = _data; first < last; ++first, ++ptr)
+      _construct(ptr, *first);
+  }
+
+
+  SWC_CAN_INLINE
+  iterator erase(const_iterator it) {
+    size_type offset = it - _data;
+    if(offset >= _size)
+      return end();
+
+    --_size;
+    pointer ptr = _data + offset;
+    for(size_type remain= _size - offset; remain; --remain, ++ptr)
+      *ptr = std::move(*(ptr + 1));
+    return _data + offset;
+  }
+
+  SWC_CAN_INLINE
+  iterator erase(const_iterator first, const_iterator last) {
+    size_type offset = first - _data;
+    if(offset >= _size)
+      return end();
+
+    size_type amt = last - first;
+    _size -= amt;
+    pointer ptr = _data + offset;
+    for(size_type remain = _size - offset; remain; --remain, ++ptr)
+      *ptr = std::move(*(ptr + amt));
+    return _data + offset;
+  }
+
+  private:
+
+
+  SWC_CAN_INLINE
+  static pointer _allocate_uinitialized(size_type size) {
+    return static_cast<pointer>(::operator new(sizeof(value_type) * size));
+    //return static_cast<pointer>(
+      //std::aligned_alloc(alignof(value_type), sizeof(value_type) * size));
+  }
+
+  /* IF can grow in-place
+  SWC_CAN_INLINE
+  static pointer _allocate_uinitialized(pointer data_prev, size_t data_prev,
+                                        size_type size) {
+    pointer data = _allocate_uinitialized(size, data_prev);
+    if(data != data_prev) {
+      _move(data, data_prev, size_prev);
+      _deallocate(data_prev, size_prev);
+    }
+    return data;
+  */
+
+  SWC_CAN_INLINE
+  static pointer _allocate_uinitialized(pointer data_prev, size_t size_prev,
+                                        size_type sz) {
+    pointer data = _move(_allocate_uinitialized(sz), data_prev, size_prev);
+    _deallocate(data_prev, size_prev);
+    return data;
+    //return static_cast<pointer>(
+    //  std::realloc(data_prev, sizeof(value_type) * sz));
+    //std::allocator<value_type> alloc;
+    //pointer data = _move(alloc.allocate(sz), data_prev, size_prev);
+    //_deallocate(data_prev, size_prev);
+    //return data;
+  }
+
+  SWC_CAN_INLINE
+  static void _deallocate(pointer data, size_t) {
+    ::operator delete(data);
+    //std::free(data);
+    //std::allocator<value_type> alloc;
+    //alloc.deallocate(data, sz);
+  }
+
+
+  SWC_CAN_INLINE
+  static pointer _allocate(size_type sz) {
+    pointer data = _allocate_uinitialized(sz);
+    for(pointer ptr = data; sz; --sz, ++ptr)
+      _construct(ptr);
+    //std::uninitialized_default_construct(ptr, ptr + sz);
+    return data;
+  }
+
+
+  template<typename... ArgsT>
+  SWC_CAN_INLINE
+  static pointer _construct(pointer ptr, ArgsT&&... args) {
+    new (ptr) value_type(std::forward<ArgsT>(args)...);
+    return ptr;
+  }
+
+
+  SWC_CAN_INLINE
+  static pointer _copy(pointer data,
+                       const_pointer data_prev, size_type size_prev) {
+    for(pointer ptr=data; size_prev; --size_prev, ++ptr, ++data_prev)
+      _construct(ptr, *data_prev);
+    //std::uninitialized_copy_n(data_prev, size_prev, data);
+    return data;
+  }
+
+  SWC_CAN_INLINE
+  static pointer _move(pointer data,
+                       pointer data_prev, size_type size_prev) {
+    for(pointer ptr=data; size_prev; --size_prev, ++ptr, ++data_prev)
+      _construct(ptr, std::move(*data_prev));
+    //std::uninitialized_move_n(data_prev, size_prev, data);
+    return data;
+  }
+
+  SWC_CAN_INLINE
+  static pointer _alter(pointer data, size_type remain, size_type amount) {
+    pointer prev = data + remain - 1;
+    for(pointer ptr = prev + amount; remain; --remain, --ptr, --prev) {
+      _construct(ptr, std::move(*prev));
+    }
+    //std::uninitialized_move_n(prev, size, data);
+    return data;
+  }
+
+
+  pointer     _data;
+  size_type   _cap;
+  size_type   _size;
+
+
+  /*
+  template<typename vT>
+  SWC_CAN_INLINE
+  T* find(const vT& value) {
+    for(auto it=Get<Iterator>(value); it; ++it) {
+      if(value == *it.item)
+        return &*it.item;
+    }
+    return nullptr;
+  }
+
+  template<typename vT>
+  SWC_CAN_INLINE
+  const T* cfind(const vT& value) {
+    for(auto it=Get<ConstIterator>(value); it; ++it) {
+      if(value == *it.item)
+        return &*it.item;
+    }
+    return nullptr;
+  }
+  */
+
+};
+
+
+}} // namespace SWC::Core
+
+
+
+#endif // swcdb_core_Vector_h
---
+++ tests/libswcdb_core/vector.cc
@@ -0,0 +1,417 @@
+/**
+ * SWC-DB© Copyright since 2019 Alex Kashirin <kashirin.alex@gmail.com>
+ * License details at <https://github.com/kashirin-alex/swc-db/#license>
+ */
+
+
+#include "swcdb/core/Exception.h"
+#include "swcdb/core/Time.h"
+#include "swcdb/core/Vector.h"
+#include <map>
+
+namespace SWC {
+
+/*
+template<typename T>
+void test_vector_huge(size_t sz) {
+  uint64_t took;
+  Time::Measure_ns track;
+
+  /// Core::Vector<T>
+  Core::Vector<T> swc_vec;
+  for(T n=sz; n; --n)
+    swc_vec.add(n);
+  SWC_ASSERT(sz == swc_vec.size());
+  took = track.elapsed();
+  std::cout << " swc_vec add: elapse=" <<  took << " avg=" << (took/sz) << std::endl;
+
+
+  track.restart();
+  for(T n=1; n <= sz; ++n) {
+    const T* p = swc_vec.cfind_(n);
+    SWC_ASSERT(p && *p == n);
+  }
+  took = track.elapsed();
+  std::cout << " swc_vec cfind_: elapse=" <<  took << " avg=" << (took/sz) << std::endl;
+
+
+  track.restart();
+  for(T n=1; n <= sz; ++n) {
+    T* p = swc_vec.find(n);
+    SWC_ASSERT(p && *p == n);
+  }
+  took = track.elapsed();
+  std::cout << " swc_vec find: elapse=" <<  took << " avg=" << (took/sz) << std::endl;
+
+
+  track.restart();
+  for(T n=1; n <= sz; ++n) {
+    const T* p = swc_vec.cfind(n);
+    SWC_ASSERT(p && *p == n);
+  }
+  took = track.elapsed();
+  std::cout << " swc_vec cfind: elapse=" <<  took << " avg=" << (took/sz) << std::endl;
+
+
+  /// std::unordered_map
+  track.restart();
+  std::unordered_map<T, T> std_unomap;
+  for(T n=sz; n; --n)
+    std_unomap[n] = n;
+  SWC_ASSERT(sz == std_unomap.size());
+  took = track.elapsed();
+  std::cout << " std_unomap add: elapse=" <<  took << " avg=" << (took/sz) << std::endl;
+
+  track.restart();
+  for(T n=1; n <= sz; ++n) {
+    auto it = std_unomap.find(n);
+    SWC_ASSERT(it != std_unomap.end() && it->second == n);
+  }
+  took = track.elapsed();
+  std::cout << " std_unomap find: elapse=" <<  took << " avg=" << (took/sz) << std::endl;
+
+
+  /// std::map
+  track.restart();
+  std::map<T, T> std_map;
+  for(T n=sz; n; --n)
+    std_map[n] = n;
+  SWC_ASSERT(sz == std_map.size());
+  took = track.elapsed();
+  std::cout << " std_map add: elapse=" <<  took << " avg=" << (took/sz) << std::endl;
+
+  track.restart();
+  for(T n=1; n <= sz; ++n) {
+    auto it = std_map.find(n);
+    SWC_ASSERT(it != std_map.end() && it->second == n);
+  }
+  took = track.elapsed();
+  std::cout << " std_map find: elapse=" <<  took << " avg=" << (took/sz) << std::endl;
+
+}
+*/
+
+template<typename VecT, typename T = VecT::value_type>
+void test_check_eq(const VecT& vec, size_t sz) {
+  auto it = vec.cbegin();
+  for(T n=sz; n; --n, ++it) {
+    if(n != *it) {
+      std::cout << "n=" << n << " *it=" << *it << std::endl;
+      SWC_ASSERT(n == *it);
+    }
+  }
+}
+
+template<typename VecT>
+void test_vector(const size_t sz) {
+
+  using T = VecT::value_type;
+
+  // push_back
+  {
+  Time::Measure_ns track;
+  VecT vec;
+  for(T n=sz; n; --n)
+    vec.push_back(n);
+  SWC_ASSERT(sz == vec.size());
+  uint64_t took = track.elapsed();
+  std::cout << typeid(VecT).name() << " cp push_back: elapse=" <<  took << " avg=" << (took/sz) << std::endl;
+  test_check_eq(vec, sz);
+  }
+
+  {
+  Time::Measure_ns track;
+  VecT vec;
+  for(T n=sz; n; --n)
+    vec.push_back(std::move(n));
+  SWC_ASSERT(sz == vec.size());
+  uint64_t took = track.elapsed();
+  std::cout << typeid(VecT).name() << " mv push_back: elapse=" <<  took << " avg=" << (took/sz) << std::endl;
+  test_check_eq(vec, sz);
+  }
+
+  // emplace_back
+  {
+  Time::Measure_ns track;
+  VecT vec;
+  for(T n=sz; n; --n)
+    vec.emplace_back(n);
+  SWC_ASSERT(sz == vec.size());
+  uint64_t took = track.elapsed();
+  std::cout << typeid(VecT).name() << " cp emplace_back: elapse=" <<  took << " avg=" << (took/sz) << std::endl;
+  test_check_eq(vec, sz);
+  }
+
+  {
+  Time::Measure_ns track;
+  VecT vec;
+  for(T n=sz; n; --n)
+    vec.emplace_back(std::move(n));
+  SWC_ASSERT(sz == vec.size());
+  uint64_t took = track.elapsed();
+  std::cout << typeid(VecT).name() << " mv emplace_back: elapse=" <<  took << " avg=" << (took/sz) << std::endl;
+  test_check_eq(vec, sz);
+  }
+
+  // cp insert at it
+  {
+  Time::Measure_ns track;
+  VecT vec;
+  for(T n=0; n < sz; )
+    vec.insert(vec.begin(), ++n);
+  SWC_ASSERT(sz == vec.size());
+  uint64_t took = track.elapsed();
+  std::cout << typeid(VecT).name() << " cp insert it: elapse=" <<  took << " avg=" << (took/sz) << std::endl;
+  test_check_eq(vec, sz);
+  }
+
+  // mv insert at it
+  {
+  Time::Measure_ns track;
+  VecT vec;
+  for(T n=0; n < sz; ) {
+    ++n;
+    vec.insert(vec.begin(), std::move(n));
+  }
+  SWC_ASSERT(sz == vec.size());
+  uint64_t took = track.elapsed();
+  std::cout << typeid(VecT).name() << " mv insert it: elapse=" <<  took << " avg=" << (took/sz) << std::endl;
+  test_check_eq(vec, sz);
+  }
+
+  // insert range at it
+  {
+  VecT vec1(sz);
+  auto it = vec1.begin();
+  for(T n=sz; n; --n, ++it)
+    *it = n;
+  SWC_ASSERT(sz == vec1.size());
+
+  Time::Measure_ns track;
+  VecT vec(1);
+  *vec.begin() = sz;
+  vec.insert(vec.begin() + 1, vec1.begin() + 1, vec1.end());
+  SWC_ASSERT(sz == vec.size());
+  uint64_t took = track.elapsed();
+  std::cout << typeid(VecT).name() << " range insert it: elapse=" <<  took << " avg=" << (took/sz) << std::endl;
+  test_check_eq(vec, sz);
+  }
+
+  // reserve & push_back
+  {
+  Time::Measure_ns track;
+  VecT vec;
+  vec.reserve(sz);
+
+  uint64_t took_resv = track.elapsed();
+  track.restart();
+  for(T n=sz; n; --n)
+    vec.push_back(n);
+  SWC_ASSERT(sz == vec.size());
+  uint64_t took = track.elapsed();
+  std::cout << typeid(VecT).name() << " resv=" << took_resv << " push_back: elapse=" <<  took << " avg=" << (took/sz) << std::endl;
+  test_check_eq(vec, sz);
+  }
+
+  // size ctor & push_back
+  {
+  Time::Measure_ns track;
+  VecT vec(sz);
+  auto it = vec.begin();
+  for(T n=sz; n; --n, ++it)
+    *it = n;
+  SWC_ASSERT(sz == vec.size());
+  uint64_t took = track.elapsed();
+  std::cout << typeid(VecT).name() << " size ctor push_back: elapse=" <<  took << " avg=" << (took/sz) << std::endl;
+  test_check_eq(vec, sz);
+  }
+
+  // assign
+  {
+  VecT vec1(sz);
+  auto it = vec1.begin();
+  for(T n=sz; n; --n, ++it)
+    *it = n;
+  SWC_ASSERT(sz == vec1.size());
+
+  Time::Measure_ns track;
+  VecT vec;
+  vec.assign(vec1.begin(), vec1.end());
+  SWC_ASSERT(sz == vec.size());
+  uint64_t took = track.elapsed();
+  std::cout << typeid(VecT).name() << " assign: elapse=" <<  took << " avg=" << (took/sz) << std::endl;
+  test_check_eq(vec, sz);
+  }
+
+  // erase it
+  {
+  VecT vec(sz);
+  auto it = vec.begin();
+  for(T n=0; n < sz; ++n, ++it)
+    *it = n;
+  SWC_ASSERT(sz == vec.size());
+
+  Time::Measure_ns track;
+  vec.erase(vec.begin() + 4);
+  SWC_ASSERT(sz - 1 == vec.size());
+  uint64_t took = track.elapsed();
+
+  it = vec.begin();
+  for(T n=0; n < sz; ++n, ++it) {
+    if(n == size_t(4)) {
+      ++n;
+      continue;
+    }
+    if(n != *it) {
+      std::cout << "n=" << n << " *it=" << *it << std::endl;
+      SWC_ASSERT(n == *it);
+    }
+  }
+  std::cout << typeid(VecT).name() << " it erase: elapse=" <<  took << " avg=" << (took/sz) << std::endl;
+  }
+
+  // erase range
+  {
+  VecT vec(sz);
+  auto it = vec.begin();
+  for(T n=0; n < sz; ++n, ++it)
+    *it = n;
+  SWC_ASSERT(sz == vec.size());
+
+  Time::Measure_ns track;
+  size_t removed = (vec.end()-3) - (vec.begin()+2);
+  vec.erase(vec.begin()+2, vec.end()-3);
+  SWC_ASSERT(sz - removed == vec.size());
+  uint64_t took = track.elapsed();
+
+  it = vec.begin();
+  for(T n=0; n < sz; ++n, ++it) {
+    if(n > size_t(1) || n < size_t(sz - 2)) {
+      ++n;
+      continue;
+    }
+    if(n != *it) {
+      std::cout << "n=" << n << " *it=" << *it << " removed=" << removed << std::endl;
+      SWC_ASSERT(n == *it);
+    }
+  }
+  std::cout << typeid(VecT).name() << " range erase: elapse=" <<  took << " avg=" << (took/sz) << std::endl;
+  }
+
+}
+
+
+
+template<typename VecT>
+void test_vector(size_t sz, uint8_t probes) {
+  std::cout << std::endl << "Testing with size=" << sz << std::endl;
+
+  Time::Measure_ns track;
+  for(uint8_t n=0; n<probes; ++n)
+    SWC::test_vector<VecT>(sz);
+  uint64_t took = track.elapsed();
+
+  std::cout << typeid(VecT).name()
+            << " sizeof=" << sizeof(VecT)
+            << " elapse=" << took
+            << " avg=" << (took/sz) << std::endl;
+}
+
+}
+
+struct Type1 {
+  Type1() noexcept { }
+  Type1(size_t sz) noexcept : sz(sz), n(std::to_string(sz)) { }
+  Type1(const Type1& other) : sz(other.sz), n(other.n) { }
+  Type1(Type1&& other) noexcept : sz(other.sz), n(std::move(other.n)) { }
+
+  Type1& operator=(const Type1& other) {
+    n = other.n;
+    sz = other.sz;
+    return *this;
+  }
+  Type1& operator=(Type1&& other) noexcept {
+    n = std::move(other.n);
+    sz = other.sz;
+    return *this;
+  }
+
+  Type1& operator=(size_t _sz) {
+    sz = _sz;
+    n = std::to_string(sz);
+    return *this;
+  }
+
+  Type1& operator--() {
+    n = std::to_string(--sz);
+    return *this;
+  }
+
+  Type1& operator++() {
+    n = std::to_string(++sz);
+    return *this;
+  }
+
+  bool operator==(const Type1& other) const noexcept {
+    return sz == other.sz && n.compare(other.n) == 0;
+  }
+
+  bool operator==( size_t other) const noexcept {
+    return sz == other;
+  }
+  bool operator<( size_t other) const noexcept {
+    return sz < other;
+  }
+  bool operator>( size_t other) const noexcept {
+    return sz > other;
+  }
+
+  std::ostream& operator<<(std::ostream& out) const {
+    out << "Type1(n=" << n << " sz=" << sz << ')';
+    return out;
+  }
+
+  const char* to_str() const noexcept {
+    return n.c_str();
+  }
+
+  operator bool() noexcept {
+    return bool(sz);
+  }
+
+  size_t sz;
+  std::string n;
+};
+
+std::ostream& operator<<(std::ostream& out, const Type1& v) {
+  return v.operator<<(out);
+}
+
+
+int main() {
+  uint8_t probes = 3;
+
+
+  // uint64_t vector
+  SWC::test_vector< std::vector<uint64_t>                 >(UINT8_MAX, probes);
+  SWC::test_vector< SWC::Core::Vector<uint64_t, uint8_t>  >(UINT8_MAX, probes);
+
+  SWC::test_vector< std::vector<uint64_t>                 >(UINT16_MAX, probes);
+  SWC::test_vector< SWC::Core::Vector<uint64_t, uint16_t> >(UINT16_MAX, probes);
+
+  //SWC::test_vector< std::vector<uint64_t>                 >(SWC::UINT24_MAX, probes);
+  //SWC::test_vector< SWC::Core::Vector<uint64_t, uint32_t> >(SWC::UINT24_MAX, probes);
+
+
+  // Type1 vector
+  SWC::test_vector< std::vector<Type1>                    >(UINT8_MAX, probes);
+  SWC::test_vector< SWC::Core::Vector<Type1, uint8_t>     >(UINT8_MAX, probes);
+
+  SWC::test_vector< std::vector<Type1>                    >(UINT16_MAX/4, probes);
+  SWC::test_vector< SWC::Core::Vector<Type1, uint16_t>    >(UINT16_MAX/4, probes);
+
+
+
+  std::cout << " OK! \n";
+  return 0;
+}
