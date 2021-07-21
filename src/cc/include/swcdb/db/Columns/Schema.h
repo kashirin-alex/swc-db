9fbb9bb4989a5663b79237f094758cd13ce3f41c
Kashirin Alex <kashirin.alex@gmail.com>
2021-07-21 15:35:31 +0300

    added DB::SchemaPrimitives + derived by DB::Schema
    changed order of class storage and serialization
    * incompatible with previous SWC-DB versions
    
    [TEST COMMIT]

--- src/cc/include/swcdb/db/Columns/Schema.h
+++ src/cc/include/swcdb/db/Columns/Schema.h
@@ -13,19 +13,177 @@
 #include "swcdb/db/Types/KeySeq.h"
 #include "swcdb/db/Types/Column.h"
 #include "swcdb/db/Types/SystemColumn.h"
+#include "swcdb/core/Serialization.h"
 
 
 namespace SWC { namespace DB {
 
 
-class Schema final {
 
+struct SchemaPrimitives {
+
+  static constexpr const cid_t     NO_CID = 0;
+
+  SWC_CAN_INLINE
+  SchemaPrimitives() noexcept
+      : cid(NO_CID),
+        revision(0),
+        cell_versions(1),
+        cell_ttl(0),
+        blk_size(0),
+        blk_cells(0),
+        cs_size(0),
+        col_seq(Types::KeySeq::LEXIC),
+        col_type(Types::Column::PLAIN),
+        blk_encoding(Types::Encoder::DEFAULT),
+        cs_replication(0),
+        cs_max(0),
+        log_rollout_ratio(0),
+        log_compact_cointervaling(0),
+        log_fragment_preload(0),
+        compact_percent(0) {
+  }
+
+  SWC_CAN_INLINE
+  SchemaPrimitives(const SchemaPrimitives& other) noexcept
+      : cid(other.cid),
+        revision(other.revision),
+        cell_versions(other.cell_versions),
+        cell_ttl(other.cell_ttl),
+        blk_size(other.blk_size),
+        blk_cells(other.blk_cells),
+        cs_size(other.cs_size),
+        col_seq(other.col_seq),
+        col_type(other.col_type),
+        blk_encoding(other.blk_encoding),
+        cs_replication(other.cs_replication),
+        cs_max(other.cs_max),
+        log_rollout_ratio(other.log_rollout_ratio),
+        log_compact_cointervaling(other.log_compact_cointervaling),
+        log_fragment_preload(other.log_fragment_preload),
+        compact_percent(other.compact_percent) {
+  }
+
+  SWC_CAN_INLINE
+  SchemaPrimitives(const uint8_t** bufp, size_t* remainp)
+    : cid(Serialization::decode_vi64(bufp, remainp)),
+      revision(Serialization::decode_vi64(bufp, remainp)),
+      cell_versions(Serialization::decode_vi32(bufp, remainp)),
+      cell_ttl(Serialization::decode_vi32(bufp, remainp)),
+      blk_size(Serialization::decode_vi32(bufp, remainp)),
+      blk_cells(Serialization::decode_vi32(bufp, remainp)),
+      cs_size(Serialization::decode_vi32(bufp, remainp)),
+      col_seq(Types::KeySeq(Serialization::decode_i8(bufp, remainp))),
+      col_type(Types::Column(Serialization::decode_i8(bufp, remainp))),
+      blk_encoding(Types::Encoder(Serialization::decode_i8(bufp, remainp))),
+      cs_replication(Serialization::decode_i8(bufp, remainp)),
+      cs_max(Serialization::decode_i8(bufp, remainp)),
+      log_rollout_ratio(Serialization::decode_i8(bufp, remainp)),
+      log_compact_cointervaling(Serialization::decode_i8(bufp, remainp)),
+      log_fragment_preload(Serialization::decode_i8(bufp, remainp)),
+      compact_percent(Serialization::decode_i8(bufp, remainp)) {
+  }
+
+  SWC_CAN_INLINE
+  uint32_t encoded_length() const noexcept {
+    return  Serialization::encoded_length_vi64(cid)
+          + Serialization::encoded_length_vi64(revision)
+          + Serialization::encoded_length_vi32(cell_versions)
+          + Serialization::encoded_length_vi32(cell_ttl)
+          + Serialization::encoded_length_vi32(blk_size)
+          + Serialization::encoded_length_vi32(blk_cells)
+          + Serialization::encoded_length_vi32(cs_size)
+          + 9;
+  }
+
+  SWC_CAN_INLINE
+  void encode(uint8_t** bufp) const {
+    Serialization::encode_vi64(bufp, cid);
+    Serialization::encode_vi64(bufp, revision);
+    Serialization::encode_vi32(bufp, cell_versions);
+    Serialization::encode_vi32(bufp, cell_ttl);
+    Serialization::encode_vi32(bufp, blk_size);
+    Serialization::encode_vi32(bufp, blk_cells);
+    Serialization::encode_vi32(bufp, cs_size);
+    Serialization::encode_i8(bufp, uint8_t(col_seq));
+    Serialization::encode_i8(bufp, uint8_t(col_type));
+    Serialization::encode_i8(bufp, uint8_t(blk_encoding));
+    Serialization::encode_i8(bufp, cs_replication);
+    Serialization::encode_i8(bufp, cs_max);
+    Serialization::encode_i8(bufp, log_rollout_ratio);
+    Serialization::encode_i8(bufp, log_compact_cointervaling);
+    Serialization::encode_i8(bufp, log_fragment_preload);
+    Serialization::encode_i8(bufp, compact_percent);
+  }
+
+  SWC_CAN_INLINE
+  void decode(const uint8_t** bufp, size_t* remainp) {
+    cid = Serialization::decode_vi64(bufp, remainp);
+    revision = Serialization::decode_vi64(bufp, remainp);
+    cell_versions = Serialization::decode_vi32(bufp, remainp);
+    cell_ttl = Serialization::decode_vi32(bufp, remainp);
+    blk_size = Serialization::decode_vi32(bufp, remainp);
+    blk_cells = Serialization::decode_vi32(bufp, remainp);
+    cs_size = Serialization::decode_vi32(bufp, remainp);
+    col_seq = Types::KeySeq(Serialization::decode_i8(bufp, remainp));
+    col_type = Types::Column(Serialization::decode_i8(bufp, remainp));
+    blk_encoding = Types::Encoder(Serialization::decode_i8(bufp, remainp));
+    cs_replication = Serialization::decode_i8(bufp, remainp);
+    cs_max = Serialization::decode_i8(bufp, remainp);
+    log_rollout_ratio = Serialization::decode_i8(bufp, remainp);
+    log_compact_cointervaling = Serialization::decode_i8(bufp, remainp);
+    log_fragment_preload = Serialization::decode_i8(bufp, remainp);
+    compact_percent = Serialization::decode_i8(bufp, remainp);
+  }
+
+  SWC_CAN_INLINE
+  bool equal(const SchemaPrimitives& other,
+             bool with_rev=true) const noexcept {
+    return  cid == other.cid &&
+            (!with_rev || revision == other.revision) &&
+            cell_versions == other.cell_versions &&
+            cell_ttl == other.cell_ttl &&
+            blk_size == other.blk_size &&
+            blk_cells == other.blk_cells &&
+            cs_size == other.cs_size &&
+            col_seq == other.col_seq &&
+            col_type == other.col_type &&
+            blk_encoding == other.blk_encoding &&
+            cs_replication == other.cs_replication &&
+            cs_max == other.cs_max &&
+            log_rollout_ratio == other.log_rollout_ratio &&
+            log_compact_cointervaling == other.log_compact_cointervaling &&
+            log_fragment_preload == other.log_fragment_preload &&
+            compact_percent == other.compact_percent
+          ;
+  }
+
+  cid_t                 cid;
+  int64_t               revision;
+  uint32_t              cell_versions;
+  uint32_t              cell_ttl;
+  uint32_t              blk_size;
+  uint32_t              blk_cells;
+  uint32_t              cs_size;
+  Types::KeySeq         col_seq;
+  Types::Column         col_type;
+  Types::Encoder        blk_encoding;
+  uint8_t               cs_replication;
+  uint8_t               cs_max;
+  uint8_t               log_rollout_ratio;
+  uint8_t               log_compact_cointervaling;
+  uint8_t               log_fragment_preload;
+  uint8_t               compact_percent;
+
+};
+
+
+
+class Schema final : public SchemaPrimitives {
   public:
 
   typedef std::shared_ptr<Schema>  Ptr;
   typedef std::vector<std::string> Tags;
-
-  static constexpr const cid_t     NO_CID = 0;
 
   SWC_CAN_INLINE
   static Ptr make() {
@@ -42,7 +200,8 @@ class Schema final {
     return Ptr(new Schema(bufp, remainp));
   }
 
-  Schema() noexcept;
+  SWC_CAN_INLINE
+  Schema() noexcept { }
 
   Schema(const Schema& other);
 
@@ -50,7 +209,7 @@ class Schema final {
 
   //~Schema() { }
 
-  bool equal(const Ptr& other, bool with_rev=true) noexcept;
+  bool equal(const Ptr& other, bool with_rev=true) const noexcept;
 
   uint32_t encoded_length() const noexcept;
 
@@ -62,31 +221,8 @@ class Schema final {
 
   void print(std::ostream& out) const;
 
-  cid_t                 cid;
   std::string           col_name;
   Tags                  tags;
-
-  Types::KeySeq         col_seq;
-  Types::Column         col_type;
-
-  uint32_t              cell_versions;
-  uint32_t              cell_ttl;
-
-  Types::Encoder        blk_encoding;
-  uint32_t              blk_size;
-  uint32_t              blk_cells;
-
-  uint8_t               cs_replication;
-  uint32_t              cs_size;
-  uint8_t               cs_max;
-
-  uint8_t               log_rollout_ratio;
-  uint8_t               log_compact_cointervaling;
-  uint8_t               log_fragment_preload;
-
-  uint8_t               compact_percent;
-
-  int64_t               revision;
 };
 
 }} // SWC::DB namespace
--- src/cc/lib/swcdb/db/Columns/Schema.cc
+++ src/cc/lib/swcdb/db/Columns/Schema.cc
@@ -5,39 +5,16 @@
 
 
 #include "swcdb/db/Columns/Schema.h"
-#include "swcdb/core/Serialization.h"
 
 
 namespace SWC { namespace DB {
 
 
-Schema::Schema() noexcept
-      : cid(NO_CID),
-        col_seq(Types::KeySeq::LEXIC), col_type(Types::Column::PLAIN),
-        cell_versions(1), cell_ttl(0),
-        blk_encoding(Types::Encoder::DEFAULT), blk_size(0), blk_cells(0),
-        cs_replication(0), cs_size(0), cs_max(0),
-        log_rollout_ratio(0),
-        log_compact_cointervaling(0),
-        log_fragment_preload(0),
-        compact_percent(0),
-        revision(0) {
+Schema::Schema(const Schema& other)
+      : SchemaPrimitives(other),
+        col_name(other.col_name), tags(other.tags) {
 }
 
-Schema::Schema(const Schema& other)
-      : cid(other.cid), col_name(other.col_name), tags(other.tags),
-        col_seq(other.col_seq), col_type(other.col_type),
-        cell_versions(other.cell_versions), cell_ttl(other.cell_ttl),
-        blk_encoding(other.blk_encoding), blk_size(other.blk_size),
-        blk_cells(other.blk_cells),
-        cs_replication(other.cs_replication), cs_size(other.cs_size),
-        cs_max(other.cs_max),
-        log_rollout_ratio(other.log_rollout_ratio),
-        log_compact_cointervaling(other.log_compact_cointervaling),
-        log_fragment_preload(other.log_fragment_preload),
-        compact_percent(other.compact_percent),
-        revision(other.revision) {
-}
 
 namespace {
 SWC_CAN_INLINE
@@ -53,103 +30,32 @@ Schema::Tags read_tags(const uint8_t** bufp, size_t* remainp) {
 }
 
 Schema::Schema(const uint8_t** bufp, size_t* remainp)
-  : cid(Serialization::decode_vi64(bufp, remainp)),
+  : SchemaPrimitives(bufp, remainp),
     col_name(Serialization::decode_bytes_string(bufp, remainp)),
-    tags(read_tags(bufp, remainp)),
-
-    col_seq(Types::KeySeq(Serialization::decode_i8(bufp, remainp))),
-    col_type(Types::Column(Serialization::decode_i8(bufp, remainp))),
-
-    cell_versions(Serialization::decode_vi32(bufp, remainp)),
-    cell_ttl(Serialization::decode_vi32(bufp, remainp)),
-
-    blk_encoding(Types::Encoder(Serialization::decode_i8(bufp, remainp))),
-    blk_size(Serialization::decode_vi32(bufp, remainp)),
-    blk_cells(Serialization::decode_vi32(bufp, remainp)),
-
-    cs_replication(Serialization::decode_i8(bufp, remainp)),
-    cs_size(Serialization::decode_vi32(bufp, remainp)),
-    cs_max(Serialization::decode_i8(bufp, remainp)),
-
-    log_rollout_ratio(Serialization::decode_i8(bufp, remainp)),
-    log_compact_cointervaling(Serialization::decode_i8(bufp, remainp)),
-    log_fragment_preload(Serialization::decode_i8(bufp, remainp)),
-
-    compact_percent(Serialization::decode_i8(bufp, remainp)),
-
-    revision(Serialization::decode_vi64(bufp, remainp)) {
-}
-
-bool Schema::equal(const Ptr& other, bool with_rev) noexcept {
-  return     cid == other->cid
-          && col_seq == other->col_seq
-          && col_type == other->col_type
-          && cell_versions == other->cell_versions
-          && cell_ttl == other->cell_ttl
-          && blk_encoding == other->blk_encoding
-          && blk_size == other->blk_size
-          && blk_cells == other->blk_cells
-          && cs_replication == other->cs_replication
-          && cs_size == other->cs_size
-          && cs_max == other->cs_max
-          && log_rollout_ratio == other->log_rollout_ratio
-          && log_compact_cointervaling == other->log_compact_cointervaling
-          && log_fragment_preload == other->log_fragment_preload
-          && compact_percent == other->compact_percent
-          && (!with_rev || revision == other->revision)
-          && Condition::str_eq(col_name, other->col_name)
-          && tags == other->tags
-          ;
+    tags(read_tags(bufp, remainp)) {
 }
 
 uint32_t Schema::encoded_length() const noexcept {
-  uint32_t sz = Serialization::encoded_length_vi32(tags.size());
+  uint32_t sz = SchemaPrimitives::encoded_length();
+  sz += Serialization::encoded_length_bytes(col_name.size());
+  sz += Serialization::encoded_length_vi32(tags.size());
   for(auto& tag : tags)
     sz += Serialization::encoded_length_bytes(tag.size());
-  return sz
-       + Serialization::encoded_length_vi64(cid)
-       + Serialization::encoded_length_bytes(col_name.size())
-       + 2
-       + Serialization::encoded_length_vi32(cell_versions)
-       + Serialization::encoded_length_vi32(cell_ttl)
-       + 1
-       + Serialization::encoded_length_vi32(blk_size)
-       + Serialization::encoded_length_vi32(blk_cells)
-       + 1
-       + Serialization::encoded_length_vi32(cs_size)
-       + 5
-       + Serialization::encoded_length_vi64(revision);
+  return sz;
 }
 
 void Schema::encode(uint8_t** bufp) const {
-  Serialization::encode_vi64(bufp, cid);
+  SchemaPrimitives::encode(bufp);
   Serialization::encode_bytes(bufp, col_name.c_str(), col_name.size());
-
   Serialization::encode_vi32(bufp, tags.size());
   for(auto& tag : tags)
     Serialization::encode_bytes(bufp, tag.c_str(), tag.size());
+}
 
-  Serialization::encode_i8(bufp, uint8_t(col_seq));
-  Serialization::encode_i8(bufp, uint8_t(col_type));
-
-  Serialization::encode_vi32(bufp, cell_versions);
-  Serialization::encode_vi32(bufp, cell_ttl);
-
-  Serialization::encode_i8(bufp, uint8_t(blk_encoding));
-  Serialization::encode_vi32(bufp, blk_size);
-  Serialization::encode_vi32(bufp, blk_cells);
-
-  Serialization::encode_i8(bufp, cs_replication);
-  Serialization::encode_vi32(bufp, cs_size);
-  Serialization::encode_i8(bufp, cs_max);
-
-  Serialization::encode_i8(bufp, log_rollout_ratio);
-  Serialization::encode_i8(bufp, log_compact_cointervaling);
-  Serialization::encode_i8(bufp, log_fragment_preload);
-
-  Serialization::encode_i8(bufp, compact_percent);
-
-  Serialization::encode_vi64(bufp, revision);
+bool Schema::equal(const Ptr& other, bool with_rev) const noexcept {
+  return  SchemaPrimitives::equal(*other.get(), with_rev) &&
+          Condition::str_eq(col_name, other->col_name) &&
+          tags == other->tags;
 }
 
 std::string Schema::to_string() const {
