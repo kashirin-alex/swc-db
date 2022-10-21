df0705311173de539501489de6957cbe7d885b26
kashirin-alex <kashirin.alex@gmail.com>
2022-10-21 23:40:58 +0300

    added test cases combinations for DUMP & LOAD
    [TEST COMMIT]

--- tests/integration/utils/utils_shell_dbclient.sh
+++ tests/integration/utils/utils_shell_dbclient.sh
@@ -3,6 +3,9 @@
 
 SWCDB_INSTALL_PATH=$1; shift
 SWCDB_SOURCE_PATH=$1; shift
+COL_TYPE=$1; shift
+COL_BY=$1; shift
+COL_EXT=$1; shift
 SWCDB_WITH_BROKER=$1; shift
 
 if [ -z $SWCDB_INSTALL_PATH ];then
@@ -13,6 +16,25 @@ if [ -z $NUM_CELLS ];then
   NUM_CELLS=1000000;
 fi
 
+if [[ $COL_BY == "NAME" ]]; then
+	col='test-shell-dbclient-1'
+else
+	col='12'
+fi
+
+if [[ $COL_TYPE == "COUNTER" ]]; then
+	gen_value="--gen-value-size=1";
+else
+	gen_value="--gen-value-size=100";
+fi
+
+if [[ $COL_EXT == "zst" ]]; then
+	fmt=' ext=zst'
+	ext='.zst'
+else
+	fmt=''
+	ext=''
+fi
 
 # START with /tmp/swcdb
 mkdir -p /tmp/swcdb;
@@ -33,32 +55,39 @@ do_test() {
 
 ## CREATE TEST DATA
 ${SWCDB_INSTALL_PATH}/bin/swcdb_load_generator -l=warn \
-  --gen-col-name="test-shell-dbclient-" \
-  --gen-cells=${NUM_CELLS} --swc.cfg.path=${SWCDB_SOURCE_PATH}/ ${SWCDB_WITH_BROKER};
+  --gen-col-name="test-shell-dbclient-" --gen-col-type=${COL_TYPE}\
+  --gen-cells=${NUM_CELLS} ${gen_value} --swc.cfg.path=${SWCDB_SOURCE_PATH}/ ${SWCDB_WITH_BROKER};
 
 
 ## DUMP 1st sample
-do_test echo "dump col='test-shell-dbclient-1' into path='dumps/dbclient/' DISPLAY_STATS; quit;" | \
+do_test echo "dump col='${col}' into path='dumps/dbclient/' ${fmt} DISPLAY_STATS; quit;" | \
+				${SWCDB_INSTALL_PATH}/bin/swcdb shell --debug --swc.cfg.path=${SWCDB_SOURCE_PATH}/ ${SWCDB_WITH_BROKER};
+do_test ls -l ${SWCDB_DATA_PATH}/dumps/dbclient/1.tsv${ext}
+
+## DELETE 1st sample
+do_test echo "delete column(name='test-shell-dbclient-1' cid=12); quit;" | \
 				${SWCDB_INSTALL_PATH}/bin/swcdb shell --debug --swc.cfg.path=${SWCDB_SOURCE_PATH}/ ${SWCDB_WITH_BROKER};
 
 
 ## LOAD 1st sample
-do_test echo "create column(name='test-shell-dbclient_load'); quit;" | \
+do_test echo "create column(name='test-shell-dbclient-1' type=${COL_TYPE} cid=12); quit;" | \
 				${SWCDB_INSTALL_PATH}/bin/swcdb shell --debug --swc.cfg.path=${SWCDB_SOURCE_PATH}/ ${SWCDB_WITH_BROKER};
 
-do_test echo "load from path='dumps/dbclient/' into col='test-shell-dbclient_load' DISPLAY_STATS; quit;" | \
+do_test echo "load from path='dumps/dbclient/' into col='${col}' DISPLAY_STATS; quit;" | \
 				${SWCDB_INSTALL_PATH}/bin/swcdb shell --debug --swc.cfg.path=${SWCDB_SOURCE_PATH}/ ${SWCDB_WITH_BROKER};
 
 
 ## DUMP 2nd sample
-do_test echo "dump col='test-shell-dbclient_load' into path='dumps/dbclient_afterload/' DISPLAY_STATS; quit;" | \
+do_test echo "dump col='${col}' into path='dumps/dbclient_afterload/' ${fmt} DISPLAY_STATS; quit;" | \
 				${SWCDB_INSTALL_PATH}/bin/swcdb shell --debug --swc.cfg.path=${SWCDB_SOURCE_PATH}/ ${SWCDB_WITH_BROKER};
+
+
 
 
 ## COMPARE 1st with 2nd sample
 
-do_test ls -l ${SWCDB_DATA_PATH}/dumps/dbclient_afterload/1.tsv
-diff -u ${SWCDB_DATA_PATH}/dumps/dbclient/1.tsv ${SWCDB_DATA_PATH}/dumps/dbclient_afterload/1.tsv;
+do_test ls -l ${SWCDB_DATA_PATH}/dumps/dbclient_afterload/1.tsv${ext}
+diff -u ${SWCDB_DATA_PATH}/dumps/dbclient/1.tsv${ext} ${SWCDB_DATA_PATH}/dumps/dbclient_afterload/1.tsv${ext};
 
 
 # STOP & CLEAR
